20c4d38e1b218a2895ffb7000f3b3a834bf9c5e7
Laurent Brusa <wrmultitudes@gmail.com>
2023-08-10 16:25:09 +0200

    first

---
+++ BasicsOfCombineWithVincent/BasicsOfCombineWithVincentApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicsOfCombineWithVincentApp.swift
+//  BasicsOfCombineWithVincent
+//
+//  Created by Laurent B on 10/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct BasicsOfCombineWithVincentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
